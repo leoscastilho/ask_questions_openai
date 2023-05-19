86f4331a9300a7a53c517c165c10869dc5bc81aa
Castilho, Leonardo <lcastilh@estee.com>
2023-05-19 16:19:10 -0300

    Add import

--- custom_components/ask_questions_openai/sensor.py
+++ custom_components/ask_questions_openai/sensor.py
@@ -7,6 +7,7 @@ import voluptuous as vol
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
 from homeassistant.const import CONF_API_KEY, CONF_NAME
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.event import async_track_state_change
 from homeassistant.core import callback
 import logging
 
